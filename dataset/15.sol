8ab00689416ac58098f0c360180e7a3c5a96bb19
zsq0702 <68530287+zsq0702@users.noreply.github.com>
2022-09-11 15:35:18 +0800

    Update 15.sol

--- dataset/15.sol
+++ dataset/15.sol
@@ -11,7 +11,7 @@ interface ERC20 {
     function approve(address _spender, uint256 _value) external returns (bool);
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
-}
+} 
  
 library SafeMath {
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {

8e0764ada418f01bb4297c4c5b6fbfcb47a94563
zsq0702 <68530287+zsq0702@users.noreply.github.com>
2022-09-11 15:38:42 +0800

    Update 35.sol

--- dataset/35.sol
+++ dataset/35.sol
@@ -8,7 +8,7 @@ library SafeMath {
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
         //require(c >= a, "SafeMath: addition overflow");
-
+ 
         return c;
     }
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
